8ae4a47bb0930eaf7aecfc62a668fa78be34a1ad
Mathias Poupon-Petit <mathias.poupon-petit@epitech.eu>
2026-04-26 12:33:31 +0200

    feat(sound): add sounds effects in game

--- barbecue-simulator/scripts/dead_zone.gd
+++ barbecue-simulator/scripts/dead_zone.gd
@@ -1,7 +1,14 @@
 extends Area2D
 
-
+@onready var music: AudioStreamPlayer = $Defeat
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "CharacterBody2D":
+		# Bloque le personnage
+		body.set_physics_process(false)
+		
+		music.play()
+		await music.finished
+		
+		body.set_physics_process(true)
 		get_tree().reload_current_scene()
